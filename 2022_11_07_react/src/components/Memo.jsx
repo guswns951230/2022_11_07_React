5ef7a159b5b9f3c720000181c7a170e6a81a7100
Kanghj <rkdguswns0803@gmail.com>
2022-11-07 17:18:19 +0900

    Memo page, add memo

--- 2022_11_07_react/src/components/Memo.jsx
+++ 2022_11_07_react/src/components/Memo.jsx
@@ -1,8 +1,49 @@
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addmemo } from "../modules/memo";
+
+import "../css/Memo.css";
+
 const Memo = () => {
+  const memolist = useSelector((state) => state.memo.memolist);
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const addMemo = useCallback(() => {
+    dispatch(addmemo({ name: name, text: text }));
+  }, [dispatch, name, text]);
+
   return (
-    <div>
+    <>
       <h1>방명록을 작성해주세요</h1>
-    </div>
+      <div className="form">
+        <label>이름</label>
+        <input
+          type="text"
+          size="10"
+          maxLength="10"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="방명록 작성"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={addMemo}>작성</button>
+      </div>
+
+      {memolist.map((memo) => (
+        <li>
+          {memo.name} : {memo.text} {memo.date}
+        </li>
+      ))}
+    </>
   );
 };
 
---
+++ 2022_11_07_react/src/modules/memo.js
@@ -0,0 +1,24 @@
+const initialState = {
+  memolist: [],
+};
+
+export const addmemo = (memo) => ({ type: "addMemo", payload: memo });
+
+const memo = (state = initialState, action) => {
+  switch (action.type) {
+    case "addMemo":
+      console.log(action.payload);
+      const newMemo = {
+        id: state.id,
+        name: action.payload.name,
+        text: action.payload.text,
+        date: `${new Date().getMonth() + 1}/${new Date().getDate()}`,
+      };
+      const newMemoList = state.memolist.concat(newMemo);
+      return { ...state, memolist: newMemoList, id: state.id + 1 };
+    default:
+      return state;
+  }
+};
+
+export default memo;
